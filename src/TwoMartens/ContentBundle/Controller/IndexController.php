e625e5e92a0c5f3f3f9b7ea09c196b4d4d843996
Jim Martens <github@2martens.de>
2015-01-13 19:44:07 +0100

    Updated to website version

--- src/TwoMartens/ContentBundle/Controller/IndexController.php
+++ src/TwoMartens/ContentBundle/Controller/IndexController.php
@@ -12,6 +12,8 @@
 namespace TwoMartens\ContentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 /**
  * Handles the index actions.
@@ -20,16 +22,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * @copyright 2013-2014 Jim Martens
  * @license   http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
  */
-class IndexController extends Controller {
-    
+class IndexController extends Controller
+{
+
     /**
      * {@inheritdoc}
      */
     public function indexAction()
     {
         return $this->render('TwoMartensContentBundle:Index:index.html.twig',
-        array(
-            'lang' => $this->get('translator')->trans('lang')
+            array(
+                'lang' => $this->get('translator')->trans('lang')
         ));
     }
-} 
+
+    /**
+     * {@inheritdoc}
+     */
+    public function webplatformAction()
+    {
+        return $this->render('TwoMartensContentBundle:Index:webplatform.html.twig');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function talksAction()
+    {
+        return $this->render('TwoMartensContentBundle:Index:talks.html.twig');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fileAction($name)
+    {
+        $filename = $this->get('kernel')->getRootDir() . '/files/'.$name.'.pdf';
+        $response = new BinaryFileResponse($filename);
+       
+        $d = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $name.'.pdf');
+        $response->headers->set('Content-Disposition', $d);
+       
+        return $response;
+    }    
+}
---
+++ web/clearCache.php
@@ -0,0 +1,13 @@
+<?php
+
+function delTree($dir)
+{
+    $files = array_diff(scandir($dir), array('.','..'));
+    foreach ($files as $file) {
+        (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+    }
+
+    return rmdir($dir);
+}
+
+delTree('../app/cache/prod/');
